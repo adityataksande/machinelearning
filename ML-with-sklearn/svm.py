8f60cfcb0373d69396f62884a872abe507c111d0
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-12 00:17:32 +0530

    Added comments

--- ML-with-sklearn/svm.py
+++ ML-with-sklearn/svm.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 from sklearn import datasets,svm
 
-digits  = datasets.load_digits()
+digits  = datasets.load_digits() # loading the data
 
-clf = svm.SVC(gamma=0.0001,C=100)
+clf = svm.SVC(gamma=0.0001,C=100)  # tuning the model
 
 print(len(digits.data))
 
-X,y = digits.data[:-1], digits.target[:-1]
+X,y = digits.data[:-1], digits.target[:-1] # getting ready with or input and target
 
-clf.fit(X,y)
+clf.fit(X,y) # fitting model
 
-print("Prediction :" , clf.predict([digits.data[-6]]))
+print("Prediction :" , clf.predict([digits.data[-6]])) # making prediction
 
-plt.imshow(digits.images[-6],cmap=plt.cm.gray_r, interpolation="nearest")
-plt.show()
+plt.imshow(digits.images[-6],cmap=plt.cm.gray_r, interpolation="nearest") # getting an image of the predicted value
+plt.show() # showing the image
