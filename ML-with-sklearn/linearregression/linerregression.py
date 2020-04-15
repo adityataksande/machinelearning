fec761739f2d595d4377fc1f0931de8a5a0a4167
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-15 18:02:38 +0530

    Added linerregression.py student.zip,etc

--- ML-with-sklearn/linearregression/linerregression.py
+++ ML-with-sklearn/linearregression/linerregression.py
@@ -15,3 +15,12 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(X_train, y_train)
+
+accuray = linear.score(X_test, y_test)
+
+print(accuray)
