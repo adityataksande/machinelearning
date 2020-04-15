e4ff0206aba1e0b4997ed3223c075b8e3b7c46cd
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-15 19:35:38 +0530

    Final linearregression.py

--- ML-with-sklearn/linearregression/linearregression.py
+++ ML-with-sklearn/linearregression/linearregression.py
@@ -20,16 +20,22 @@ predict = "G3"
 
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
 
-linear.fit(X_train, y_train)
+'''
+best = 0
+for _ in range(30):
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+    linear = linear_model.LinearRegression()
+    linear.fit(X_train, y_train)
+    accuray = linear.score(X_test, y_test)
+    print(accuray)
 
-#creating the pickle
-with open("studentmodel.pickle","wb") as f:
-    pickle.dump(linear, f)
+    if accuray>best:
+        best=accuray
+        with open("studentmodel.pickle","wb") as f:
+            pickle.dump(linear, f)'''
 
 pickle_in = open("studentmodel.pickle","rb")
 linear = pickle.load(pickle_in)
@@ -46,3 +52,13 @@ print("Intercept: \n",linear.intercept_)
 predictions = linear.predict(X_test)
 for x in range(len(predictions)):
     print(predictions[x], X_test[x], y_test[x])
+
+
+
+    # ploting
+p = "G2"
+style.use("ggplot")
+plt.scatter(data[p],data["G3"])
+plt.xlabel(p)
+plt.ylabel("Final Garde")
+plt.show()
