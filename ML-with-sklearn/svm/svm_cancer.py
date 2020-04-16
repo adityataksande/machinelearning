d386c9ee1130a663014f09a4b11ad68116c033fe
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-16 17:15:01 +0530

    Created svm_cancer.py

---
+++ ML-with-sklearn/svm/svm_cancer.py
@@ -0,0 +1,11 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+
+cancer = datasets.load_breast_cancer()
+
+# print(dir(cancer))
+# print(cancer.feature_names)
+# print(cancer.target_names)
+
+
