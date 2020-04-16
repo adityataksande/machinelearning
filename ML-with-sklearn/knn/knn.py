9b3c9561649a7cc504d0ae5e829b89596d35f058
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-16 15:01:42 +0530

    Final KNN(K Nearest Neighbour) Algorithm

--- ML-with-sklearn/knn/knn.py
+++ ML-with-sklearn/knn/knn.py
@@ -9,3 +9,43 @@ df = pd.read_csv("car.data")
 print(df.head())
 
 # getting dummies done
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(df["buying"]))
+maint = le.fit_transform(list(df["maint"]))
+door = le.fit_transform(list(df["door"]))
+persons = le.fit_transform(list(df["persons"]))
+lug_boot = le.fit_transform(list(df["lug_boot"]))
+safety = le.fit_transform(list(df["safety"]))
+cls = le.fit_transform(list(df["class"]))
+# print(buying)
+
+
+predict = "class"
+
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+
+y = list(cls)
+
+
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+# print(X_train,y_test)
+
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(X_train,y_train)
+
+acc= model.score(X_test,y_test)
+print(acc)
+
+predicted = model.predict(X_test)
+names = ["unacc", "acc", "good", "verygood"]
+
+for x in range(len(X_test)):
+    print("Predicted : ",predicted[x], "Data: ", X_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([X_test[x]], 9, True)
+    print(n)
+
