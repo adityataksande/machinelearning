406e5fbf590fad6dbc9993f41c93a2a0f015d7de
Aditya Taksande <taksandeaditya@gmail.com>
2020-04-16 17:23:58 +0530

    Update svm_cancer.py

--- ML-with-sklearn/svm/svm_cancer.py
+++ ML-with-sklearn/svm/svm_cancer.py
@@ -9,3 +9,23 @@ cancer = datasets.load_breast_cancer()
 # print(cancer.target_names)
 
 
+X = cancer.data
+y = cancer.target
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+
+print(X_train,y_train)
+
+classes = ['malignant' 'benign']
+
+
+clf = svm.SVC(kernel='linear')
+
+clf.fit(X_train, y_train)
+
+
+y_predict = clf.predict(X_test)
+
+acc = sklearn.metrics.accuracy_score(y_test, y_predict)
+
+print(acc)
